6aaa75a2cfb8dcd210b36b9285aca9f2ab9856db
kita456 <g140702@gmail.com>
2025-06-21 11:36:40 +0900

    add

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import { TermsOfServicePage } from './pages/TermsOfService.page';
 
-const basename = import.meta.env.MODE === 'production' ? '/kube-i18n-status' : '/';
+const basename = import.meta.env.MODE === 'production' ? '/kubernetes-i18n-tracker' : '/';
 
 const router = createBrowserRouter(
   [
