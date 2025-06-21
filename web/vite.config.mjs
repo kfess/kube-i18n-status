5c21a75fd0e440da59c479edc680d3c23aee93ca
kita456 <g140702@gmail.com>
2025-06-21 11:24:13 +0900

    add

--- web/vite.config.mjs
+++ web/vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const repoName = '/kube-i18n-status/';
+const repoName = '/kubernetes-i18n-tracker/';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
