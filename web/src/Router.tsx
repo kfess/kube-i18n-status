be912bdab9a7abb3079507a2fdba00d97196b0b7
kita456 <g140702@gmail.com>
2025-06-25 00:59:10 +0900

    chore: remove import

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -1,15 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
-import { TermsOfServicePage } from './pages/TermsOfService.page';
 
 const basename = import.meta.env.MODE === 'production' ? '/kubernetes-i18n-tracker' : '/';
 
-const router = createBrowserRouter(
-  [
-    { path: '/', element: <HomePage /> },
-  ],
-  { basename }
-);
+const router = createBrowserRouter([{ path: '/', element: <HomePage /> }], { basename });
 
 export function Router() {
   return <RouterProvider router={router} />;
