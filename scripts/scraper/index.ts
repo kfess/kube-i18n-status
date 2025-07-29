25ce307baed6659ec80f2cd7990e76e44feb94ea
kfess <35001425+kfess@users.noreply.github.com>
2025-07-29 15:58:33 +0900

    Feature/issue 41 analytics dashboard (#42)
    * feature: add playwright and scraping script
    
    * feature: View, New Users でソートできるように修正
    
    * feature: Add playwright scraper (fetch kubernetes analytics data)

---
+++ scripts/scraper/index.ts
@@ -0,0 +1,57 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { chromium } from "playwright";
+
+const url =
+  "https://lookerstudio.google.com/u/0/reporting/fe615dc5-59b0-4db5-8504-ef9eacb663a9/page/p_ognozybded";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const savePath = path.resolve(__dirname, "../../data/master/page_view.csv");
+
+const scrape = async () => {
+  const browser = await chromium.launch({ headless: true });
+  const context = await browser.newContext({ acceptDownloads: true });
+  const page = await context.newPage();
+
+  try {
+    await page.goto(url);
+    await page.waitForTimeout(10000);
+    console.log("Page loaded");
+
+    const mainArea = page.locator("body");
+    const box = await mainArea.boundingBox();
+
+    if (!box) {
+      throw new Error(
+        "Bounding box not found. Ensure the page is loaded correctly."
+      );
+    }
+
+    const centerX = box.x + box.width / 2;
+    const centerY = box.y + box.height / 2;
+    await page.mouse.click(centerX, centerY, { button: "right" });
+    console.log("Right-clicked");
+
+    await page.locator("text=エクスポート").first().click();
+    console.log("Clicked context menu export");
+
+    await page.locator("text=エクスポート").nth(1).click();
+    console.log("Clicked submenu export");
+
+    const [download] = await Promise.all([
+      page.waitForEvent("download", { timeout: 15000 }),
+      page.locator('button:has-text("エクスポート")').click(),
+    ]);
+
+    await download.saveAs(savePath);
+    console.log("Download completed");
+  } catch (err: unknown) {
+    console.error("Error:", (err as Error).message);
+  } finally {
+    await browser.close();
+  }
+};
+
+scrape();
