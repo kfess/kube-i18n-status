ac8d2362fd691bec28b6077cbab1070c1ebfc0cb
kita456 <g140702@gmail.com>
2025-08-11 09:45:12 +0000

    add read PR

--- scripts/python/log.py
+++ scripts/python/log.py
@@ -1,23 +1,5 @@
 import logging
-import time
 
-
-def _jst_converter(timestamp: int) -> time.struct_time:
-    """Convert a timestamp to Japan Standard Time (JST).
-
-    Args:
-    ----
-        timestamp (int): The timestamp to convert.
-
-    Returns:
-    -------
-        time.struct_time: The converted time in JST.
-
-    """
-    return time.localtime(timestamp + 9 * 3600)
-
-
-logging.Formatter.converter = _jst_converter
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s JST - %(levelname)s - %(message)s",
